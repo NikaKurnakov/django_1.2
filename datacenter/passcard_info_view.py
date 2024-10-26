ed819823b6642eec34cf0a4925a486efd69e4a0e
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2024-10-26 14:41:33 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,20 +1,19 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
-from datacenter.models import is_visit_long
+from django.shortcuts import render, get_object_or_404
+from datacenter.models import is_visit_long, get_duration
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.get_object_or_404(passcode=passcode)
+    passcard = get_object_or_404(Passcard, passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
     this_passcard_visits = []
-
     for visit in visits:
         this_passcard_visits.append(
             {
                 'entered_at': visit.entered_at,
-                'duration': is_visit_long(visit),
-                'is_strange': False
+                'duration': get_duration(visit),
+                'is_strange': is_visit_long(visit)
             })
 
     context = {
